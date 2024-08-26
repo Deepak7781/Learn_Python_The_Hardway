37613f80553b34d8e409a4c07dab2284505f8169
deepak <deepak21082005@gmail.com>
2024-08-26 20:38:46 +0530

    ex1-ex17

---
+++ ex1.py
@@ -0,0 +1,7 @@
+print("Hello World!")
+print("Hello Pyton")
+print("I like typing this.")
+print("Yay This is fun.")
+print("Printing..")
+print("I'd much rather you 'not'")
+print("I said do not touch this")
---
+++ ex5.py
@@ -0,0 +1,14 @@
+name = "Deepak"
+age = 19
+height = 173 #cm
+weight = 60 #kg
+eyes = "Black"
+hair = "Black"
+print(f"Let's talk about {name}")
+print(f"He's {age} years old.")
+print(f"He's {height} cm tall")
+print(f"He's {weight} kg heavy")
+print(f"He's got {eyes} eyes and {hair} hair")
+
+total = age + height + weight
+print(f"If I add {age}, {height}, and {weight} I get {total}")
